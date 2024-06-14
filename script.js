f2dbf6796adaae713a3947621d7b338808dc06d9
Partha Batabyal <163519640+Partha-Batabyal@users.noreply.github.com>
2024-06-14 12:48:14 +0530

    --v1
    small feature added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let change = take(".change");
 let left = take("#left");
 let right = take("#right");
 
+let color_toggler = take(".box3_color>.container>.toggle>input");
+
+
 // Make it full screen first
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -15,20 +18,40 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 change_theme.checked = true;
 
+
+
 change_theme.addEventListener("change", () => {
-  if (change_theme.checked == false) {
+  if (change_theme.checked === false) {
     left.style.width = "0";
     right.style.marginLeft = "0";
     right.style.width = "100%";
     change.style.marginLeft = "4%";
+    typed.reset();
+    typed.stop();
   } else {
     left.style.width = "20%";
     right.style.marginLeft = "20%";
     right.style.width = "80%";
     change.style.marginLeft = "-1.3%";
+    typed1();
+
   }
 });
 
-document.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
+color_toggler.addEventListener("change", () => {
+  if (color_toggler.checked == false) {
+    console.log("hello");
+    document.body.classList.toggle("mycolor");
+  } else {
+    console.log("huu");
+    document.body.classList.toggle("mycolor");
+  }
 });
+
+
+
+
+
+// document.addEventListener("contextmenu", (e) => {
+//   e.preventDefault();
+// });
---
+++ typing.js
@@ -0,0 +1,13 @@
+
+let typed1 = () => {
+
+  typed = new Typed(".typing_left", {
+    strings: ["Hii", "It's", "Your", "Brain"],
+    typeSpeed: 150,
+    backSpeed: 15,
+    loop: true,
+    showCursor: false,
+  });
+}
+
+// typed1();
